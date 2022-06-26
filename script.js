0015ba591ac1dfe0567ff6c14df70308522b79b5
Invincible-ibeh <miradmayor@gmail.com>
2022-06-26 17:30:54 -0500

    More Changes

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ function settingsMenuToggle(){
 darkBtn.onclick = function(){
     darkBtn.classList.toggle("dark-btn-on");
     document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme")=="light"){
+        localStorage.setItem("theme", "dark");
+    }
+    else{
+        localStorage.setItem("theme", "light")
+    }
 }
 
 
